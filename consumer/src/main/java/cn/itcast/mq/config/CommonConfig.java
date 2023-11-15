aab32a0922741e219fd73bab01370e85a909cbb8
root <147520338+root-akina@users.noreply.github.com>
2023-11-15 09:49:12 +0800

    RabbitMQ-消息从路由传递到队列失败

--- consumer/src/main/java/cn/itcast/mq/config/CommonConfig.java
+++ consumer/src/main/java/cn/itcast/mq/config/CommonConfig.java
@@ -1,15 +1,30 @@
 package cn.itcast.mq.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
-public class CommonConfig {
-
+public class CommonConfig implements ApplicationContextAware {
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        //1.获得RabbitMqTemplate
+        RabbitTemplate rabbitTemplate = applicationContext.getBean(RabbitTemplate.class);
+        //2.设置ReturnCallback
+        rabbitTemplate.setReturnCallback(new RabbitTemplate.ReturnCallback() {
+            @Override
+            public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
+                log.info("消息从路由传递到队列失败，应答码{}，原因{}，交换机{}，路由键{},消息{}",
+                        replyCode,replyText,exchange,routingKey,message);
+            }
+        });
+    }
 }
